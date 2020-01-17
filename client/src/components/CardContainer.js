971c7ba83a463ee297e1d0068efa4125cfcb5692
Vincent Sanders <vincesanders@hotmail.com>
2020-01-17 12:05:56 -0600

    Added Bar graph object to CardContainer. Added graphData object with the default graph data I wanted. Added the componentDidUpdate method to Card Container to check if the state of the data has changed. If the data has changed, I added functionality to populate the graph data with the new data.

--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import axios from 'axios';
 import PlayerCard from './PlayerCard';
+import { Bar } from 'react-chartjs-2';
 
 class CardContainer extends React.Component {
     state = {data: []};
+    
+    graphData = {
+        labels: [],
+        datasets: [{
+            label: '# of Searches',
+            data: [],
+            backgroundColor: [
+                'rgba(102, 0, 0, 0.2)',
+                'rgba(102, 0, 51, 0.2)',
+                'rgba(102, 0, 102, 0.2)',
+                'rgba(51, 0, 102, 0.2)',
+                'rgba(0, 0, 102, 0.2)',
+                'rgba(0, 51, 102, 0.2)',
+                'rgba(0, 102, 255, 0.2)',
+                'rgba(0, 102, 102, 0.2)',
+                'rgba(0, 102, 51, 0.2)',
+                'rgba(51, 102, 0, 0.2)',
+                'rgba(255, 204, 102, 0.2)',
+                'rgba(102, 102, 0, 0.2)',
+                'rgba(102, 77, 0, 0.2)',
+                'rgba(102, 51, 0, 0.2)'
+            ], 
+            borderColor: [
+                'rgba(102, 0, 0, 1)',
+                'rgba(102, 0, 51, 1)',
+                'rgba(102, 0, 102, 1)',
+                'rgba(51, 0, 102, 1)',
+                'rgba(0, 0, 102, 1)',
+                'rgba(0, 51, 102, 1)',
+                'rgba(0, 102, 255, 1)',
+                'rgba(0, 102, 102, 1)',
+                'rgba(0, 102, 51, 1)',
+                'rgba(51, 102, 0, 1)',
+                'rgba(255, 204, 102, 1)',
+                'rgba(102, 102, 0, 1)',
+                'rgba(102, 77, 0, 1)',
+                'rgba(102, 51, 0, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
 
     componentDidMount() {
         axios.get('http://localhost:5000/api/players')
@@ -24,9 +66,22 @@ class CardContainer extends React.Component {
             });
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.data !== prevState.data) {
+            this.state.data.forEach(playerObj => {
+                if (!this.graphData.labels.includes(playerObj.country)) {
+                    this.graphData.labels.push(playerObj.country);
+                }
+                this.graphData.datasets[0].data[this.graphData.labels.indexOf(playerObj.country)] = this.graphData.datasets[0].data[this.graphData.labels.indexOf(playerObj.country)] || 0 + parseInt(playerObj.searches);
+            })
+        }
+    }
+
     render() {
+        console.log(this.graphData);
         return (
             <div className='cards-container'>
+                <Bar data={this.graphData} />
                 {/* map through state data to create cards for each player with info. */}
                 {this.state.data.map(player => {
                     return <PlayerCard name={player.name} searches={player.searches} country={player.country} key={player.id} />
