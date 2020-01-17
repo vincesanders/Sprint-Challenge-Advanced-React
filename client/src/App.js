3c2552048c51eb174a8356b7ddf36009ddbdf935
Vincent Sanders <vincesanders@hotmail.com>
2020-01-17 11:09:59 -0600

    Added useDarkMode and useLocalStorage hools.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import CardContainer from './components/CardContainer';
 import Navbar from './components/Navbar';
+import useDarkMode from './hooks/useDarkMode';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  }
+
   return (
     <div className="App">
       <Navbar toggleMode={toggleMode} darkMode={darkMode} />
