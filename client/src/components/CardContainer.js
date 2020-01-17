a92ce8d16186960b818fbc1fdf48ad26c1c814a5
Vincent Sanders <vincesanders@hotmail.com>
2020-01-17 10:23:20 -0600

    Added basic scaffolding of CardContainer and axios call. Added a basic sort function to arrange the data from most searches to least.

--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 class CardContainer extends React.Component {
+    state = {data: []};
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/players')
+            .then(res => {
+                //res.data is an array of 100 soccerplayers, with name, country, searches and id
+                //sort the array based on number of searches.
+                this.setState({data: res.data.sort((a, b) => {
+                    if (a.searches > b.searches) {
+                        return -1;
+                    }
+                    if (a.searches < b.searches) {
+                        return 1;
+                    }
+                    return 0;
+                })});
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+    
     render() {
         return (
             <div className='cards-container'>
