9f5f9b1f51fe022da07646de3d56428153ad3931
Vincent Sanders <vincesanders@hotmail.com>
2020-01-17 10:20:07 -0600

    Created CardContainer component and added to App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CardContainer from './components/CardContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <CardContainer />
     </div>
   );
 }
---
+++ client/src/components/CardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CardContainer extends React.Component {
+    render() {
+        return (
+            <div className='cards-container'>
+                {/* map through state data to create cards for each player with info. */}
+            </div>
+        );
+    }
+}
